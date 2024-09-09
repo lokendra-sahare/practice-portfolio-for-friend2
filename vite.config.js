d2b8bb76c40a8584854422a1b049bcf66967b0bf
lokendra-sahare <lokendrasahare@gmail.com>
2024-09-09 10:56:12 +0530

    Update Vite config for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 3000, // Use the environment variable PORT or default to 3000
+    host: "0.0.0.0", // Bind to all IP addresses so it's accessible externally
+  },
+});
